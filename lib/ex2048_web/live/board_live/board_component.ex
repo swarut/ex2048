4447c6c84ef2be6d58310b0a9da84f75a2845740
Warut Surapat <s.warut@gmail.com>
2020-04-29 00:50:54 +0900

    Trigger key event from board.

--- lib/ex2048_web/live/board_live/board_component.ex
+++ lib/ex2048_web/live/board_live/board_component.ex
@@ -3,7 +3,7 @@ defmodule Ex2048Web.BoardLive.BoardComponent do
 
   def render(assigns) do
     ~L"""
-    <div id="board">
+    <div id="board" phx-window-keyup="update_board" phx-target="<%= @myself %>">
       <%= for cell <- @cells do %>
         <div class="item item-<%= cell %>">
           <div class="cell">
@@ -13,5 +13,10 @@ defmodule Ex2048Web.BoardLive.BoardComponent do
       <% end %>
     </div>
     """
+  end
+
+  def handle_event("update_board", %{"code" => key_code}, socket) do
+    IO.puts("========== #{key_code} was pressed")
+    {:noreply, socket |> assign(:key, "pressed")}
   end
 end
--- lib/ex2048_web/live/board_live/index.ex
+++ lib/ex2048_web/live/board_live/index.ex
@@ -6,12 +6,25 @@ defmodule Ex2048Web.BoardLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:boards, fetch_boards()) |> assign(:cells, Enum.to_list(1..16))}
+    {:ok,
+     socket
+     |> assign(:boards, fetch_boards())
+     |> assign(:cells, Enum.to_list(1..16))
+     |> assign(:key, "nothing")}
   end
 
   @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => _id}, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("update_board", %{"id" => _id}, socket) do
+    {:noreply, socket}
   end
 
   defp apply_action(socket, :index, _params) do
